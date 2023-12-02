6e036ea28aec0cf9e8d43cb0fe8f65e001b673e3
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-02 10:57:42 +0530

    Adding the dat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,23 @@
     import { onMount } from 'svelte';
     import Tesseract from 'tesseract.js';
 
+    // Variables to store the selected image and the recognized text
     let selectedImage = null;
-    let recognizedText = null;
+    let recognizedText = 'Image not uploaded yet';
 
+    // Function to handle the image upload
     const handleImageUpload = (event) => {
       const image = event.target.files[0];
       selectedImage = URL.createObjectURL(image);
     };
   
+    // Function to recognize text in the selected image
     const recognizeText = async () => {
       const { data: { text } } = await Tesseract.recognize(selectedImage, 'eng');
       recognizedText = text;
     };
 
     // when the selectedImage variable changes, run the recognizeText function
-
     $: if (selectedImage) {
       recognizeText();
     }
@@ -31,13 +33,43 @@
         }
       };
     });
+
+    onMount(() => {
+        const button = document.querySelector('button');
+        const input = document.querySelector('input');
+        button.addEventListener('click', () => {
+            input.click();
+        });
+    });
+
   </script>
   
   <style>
     /* Add your styles here */
+    img {
+        width: 200px;
+        height: 200px;
+    }
+
+    div {
+        height: 100vh;
+        widows: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    input {
+        width: 50px;
+        height: 50px;
+        display: none;
+    }
+
   </style>
   
   <div>
+    <button>upload image</button>
     <input type="file" accept="image/*" on:change={handleImageUpload} />
     {#if selectedImage}
       <img src={selectedImage} alt="Selected" />
