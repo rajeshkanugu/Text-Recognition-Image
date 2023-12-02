30a9bf06051767dd31ca6be363006de7a0604e3a
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-02 10:43:03 +0530

    Adding the text recognition model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    // @ts-nocheck
+    import { onMount } from 'svelte';
+    import Tesseract from 'tesseract.js';
+
+    let selectedImage = null;
+    let recognizedText = null;
+
+    const handleImageUpload = (event) => {
+      const image = event.target.files[0];
+      selectedImage = URL.createObjectURL(image);
+    };
+  
+    const recognizeText = async () => {
+      const { data: { text } } = await Tesseract.recognize(selectedImage, 'eng');
+      recognizedText = text;
+    };
+
+    // when the selectedImage variable changes, run the recognizeText function
+
+    $: if (selectedImage) {
+      recognizeText();
+    }
+
+    // Cleanup when the component is destroyed to prevent memory leaks
+    onMount(() => {
+      return () => {
+        if (selectedImage) {
+          URL.revokeObjectURL(selectedImage);
+        }
+      };
+    });
+  </script>
+  
+  <style>
+    /* Add your styles here */
+  </style>
+  
+  <div>
+    <input type="file" accept="image/*" on:change={handleImageUpload} />
+    {#if selectedImage}
+      <img src={selectedImage} alt="Selected" />
+    {/if}
+    <p>{recognizedText}</p>
+  </div>
+  
